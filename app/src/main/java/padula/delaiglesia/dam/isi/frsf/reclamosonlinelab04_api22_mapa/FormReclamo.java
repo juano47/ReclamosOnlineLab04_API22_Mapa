93917bdd13ffca491e37c24e73402eec52af0a88
Nicolas <nicolasvpadula@gmail.com>
2017-11-20 16:44:59 -0300

    Implementa funcionalidad de capturar y almacenar fotos

--- app/src/main/java/padula/delaiglesia/dam/isi/frsf/reclamosonlinelab04_api22_mapa/FormReclamo.java
+++ app/src/main/java/padula/delaiglesia/dam/isi/frsf/reclamosonlinelab04_api22_mapa/FormReclamo.java
@@ -1,19 +1,30 @@
 package padula.delaiglesia.dam.isi.frsf.reclamosonlinelab04_api22_mapa;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
@@ -32,14 +43,18 @@ public class FormReclamo extends AppCompatActivity {
     Button btnelegirLugar;
     Button btnCancelar;
     Button btnEliminar;
+    Button btnCapturarFoto;
+    ImageView fotoImgView;
     ReclamoDaoHTTP daoHTTP = new ReclamoDaoHTTP();
     List<TipoReclamo> tipoReclamos;
     ArrayAdapter adapterTiposReclamos;
     Integer req;
     public static Integer LUGAR_FROM_MAPA = 3432;
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
     Reclamo nuevoReclamo = new Reclamo();
 
     private GoogleMap mapa;
+    private Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +69,9 @@ public class FormReclamo extends AppCompatActivity {
         frmReclamoDetalle = (EditText) findViewById(R.id.frmReclamoTextDetReclamo);
         frmReclamoSpinner = (Spinner) findViewById(R.id.frmReclamoCmbTipo);
         lugarReclamoTextView = (TextView) findViewById(R.id.frmReclamoLblLugar);
+        btnCapturarFoto = (Button) findViewById(R.id.btnCapturarFoto);
+        fotoImgView = (ImageView) findViewById(R.id.frmReclamoImgFoto);
+
 
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -84,6 +102,7 @@ public class FormReclamo extends AppCompatActivity {
 
                 frmReclamoSpinner.setSelection(pos);
 
+                checkAndRetrieveImage();
             }
             else{
                 btnEliminar.setEnabled(false);
@@ -132,6 +151,11 @@ public class FormReclamo extends AppCompatActivity {
                 }
 
 
+                if(imageBitmap != null)
+                    guardarImagenEnFS();
+
+
+
                 Intent resultado = getIntent();
                 resultado.putExtra("RECLAMO",nuevoReclamo );
                 resultado.putExtra("OPERACION","EDITAR");
@@ -154,6 +178,69 @@ public class FormReclamo extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        btnCapturarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+
+            }
+        });
+    }
+
+    private void checkAndRetrieveImage() {
+        Integer id = nuevoReclamo.getId();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+        //File directory = getApplicationContext().getDir("imagenes-dam/reclamo_" + id + ".jpg",Context.MODE_PRIVATE);
+        File directory = getApplicationContext().getDir("imagenes-dam",Context.MODE_PRIVATE);
+        File imgFile = new File(directory,"reclamo_" + nuevoReclamo.getId() + ".jpg");
+        // Create imageDir
+        if(imgFile.exists()){
+            try {
+                imageBitmap = BitmapFactory.decodeStream(new FileInputStream(imgFile), null, options);
+                fotoImgView.setImageBitmap(imageBitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void guardarImagenEnFS() {
+        File directory = getApplicationContext().getDir("imagenes-dam",Context.MODE_PRIVATE);
+        FileOutputStream output = null;
+        // Create imageDir
+        if(!directory.exists())
+            directory.mkdir();
+
+        File imgPath = new File(directory,"reclamo_" + nuevoReclamo.getId() + ".jpg");
+
+        try {
+            output = new FileOutputStream(imgPath);
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG,100,output);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally{
+
+                if (output != null)
+                    try {
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
     }
 
 
@@ -161,7 +248,13 @@ public class FormReclamo extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             if((requestCode == LUGAR_FROM_MAPA) && (resultCode == RESULT_OK)){
                 recibirLugarDesdeMapa(data);
-        }
+            }
+            else if((requestCode == REQUEST_IMAGE_CAPTURE) && (resultCode == RESULT_OK)){
+                Bundle extras = data.getExtras();
+                imageBitmap = (Bitmap) extras.get("data");
+                fotoImgView.setImageBitmap(imageBitmap);
+
+            }
     }
 
     private void recibirLugarDesdeMapa(Intent data) {
